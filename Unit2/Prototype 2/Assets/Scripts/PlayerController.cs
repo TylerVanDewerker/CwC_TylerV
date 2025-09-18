f35ab8aafb7f07957684c98fbd2e318e75dec223
TylerVanDewerker <Tyler.VanDewerker@s.capregboces.org>
2025-09-18 09:42:46 -0400

    finished 2.2 with a bit of 2.3
    finished 2.2 with a bit of 2.3

--- Unit2/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit2/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     float xRange = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject projectilePrefab;
     void Start()
     {
         
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //set up a barrier to block the player from moving outside of a certain range
         if(transform.position.x < -xRange)
         {
@@ -26,5 +28,9 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
